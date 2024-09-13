2bfab579b9124b6d911c5195e985c6d529375c9a
sathishkumar <sathis.m76@gmail.com>
2024-09-13 15:10:44 +0530

    design part completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <nav className="navbar">
+      <div className="navbar-left">
+        <a href="/" className="logo">
+          College
+        </a>
+      </div>
+      <div className="navbar-center">
+        <ul className="nav-links">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/dasboard">Dasboard</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="/register">Register</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
+import React from "react";
 import NavBar from "../components/NavBar";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <>
       <NavBar />
-      <h1>home</h1>
+      <div>Dashboard</div>
     </>
   );
 }
